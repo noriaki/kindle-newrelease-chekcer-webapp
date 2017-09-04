96656720ceae985c557f04dd7f079c4fe7949c86
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-09-04 16:33:09 +0900

    add logic to book,user model

--- db/models/book.js
+++ db/models/book.js
@@ -1,5 +1,8 @@
+const moji = require('moji');
 const { Schema } = require('mongoose');
-const getBooks = require('../../../lib/amazon/getBooks');
+
+const connection = require('../').createConnection();
+const getBooks = require('../../lib/amazon/getBooks');
 
 const bookSchema = new Schema({
   asin: {
@@ -19,10 +22,12 @@ const bookSchema = new Schema({
     default: false,
   },
   title: String,
+  titleReading: String,
   description: String,
   url: String,
   image: String,
   authors: Array,
+  authorsReading: String,
   publisher: String,
   publishedAt: Date,
   releasedAt: Date,
@@ -30,7 +35,7 @@ const bookSchema = new Schema({
   timestamps: true,
 });
 
-class Book {
+class BookClass {
   static async firstOrCreate(query, doc = query) {
     const book = await this.findOne(query);
     if (book) { return { book, newRecord: false }; }
@@ -62,18 +67,31 @@ class Book {
       .limit(10);
   }
 
-  static createByAmazonApiResult({
-    asin,
-    authors,
-    productImage,
-    sortableAuthors,
-    sortableTitle,
-    title,
+  static async createOrUpdateByMyxItem({
+    asin, authors, productImage, sortableAuthors, sortableTitle, title,
   }) {
+    const params = {
+      authors: extractAuthors(authors),
+      authorsReading: sortableAuthors,
+      image: productImage,
+      title,
+      titleReading: sortableTitle,
+    };
+    let book = await this.findOne({ asin });
+    if (book == null) { book = new this({ asin }); }
+    return await book.set(params).save();
   }
 }
-bookSchema.loadClass(Book);
+bookSchema.loadClass(BookClass);
 
-module.exports = {
-  bookSchema,
-};
+const Book = connection.model('Book', bookSchema);
+Book.shcema = bookSchema;
+
+module.exports = Book;
+
+const extractAuthors = authors => (
+  moji(authors)
+    .convert('ZStoHS').toString()
+    .split(/[、､，,]+\s*/g)
+    .map(author => author.replace(/\s+/g, ''))
+);
