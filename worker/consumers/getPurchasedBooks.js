7ea4a27bd4e28e8f0e11a9b56213638f524f1bae
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-09-05 09:48:55 +0900

    add pushing queue of getting book info

--- worker/consumers/getPurchasedBooks.js
+++ worker/consumers/getPurchasedBooks.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
+const chunk = require('lodash.chunk');
 
 const getPurchasedBooks = require('../../lib/amazon/getPurchasedBooks');
 const User = require('../../db/models/user');
 const cert = process.env.SECRET_KEY_BASE;
+
+const exchange = require('../amqp')();
+const key = 'books.detail.get';
 
 const options = { name: 'books.purchased.get', durable: true };
 const handler = ({ dataToken }, ack) => {
@@ -11,6 +15,11 @@ const handler = ({ dataToken }, ack) => {
     const user = await User.findOne({ identifier });
     const books = await getPurchasedBooks(aid, apw);
     await user.createBooksPurchased(books);
+    chunk(books, 10).map((items) => {
+      const asins = items.map(item => item.asin);
+      // console.log('processing to [%s]... %s', key, asins.join());
+      exchange.publish({ asins }, { key });
+    });
     ack();
   })();
 };
