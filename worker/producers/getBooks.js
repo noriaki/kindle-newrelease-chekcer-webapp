a9d49f19b703cd3bcb7b3e831ddc7ecbdbc499a5
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-09-05 09:52:48 +0900

    add producer script that inactive books are retrieved and updated

---
+++ worker/producers/getBooks.js
@@ -0,0 +1,23 @@
+const Book = require('../../db/models/book');
+const exchange = require('../amqp')();
+
+const key = 'books.detail.get';
+
+(async () => {
+  let books = await Book.whereNeedsUpdate().exec();
+  while (books != null && books.length > 0) {
+    const asins = books.map(book => book.asin);
+    console.log('processing to [%s]... %s', key, asins.join());
+    await Book.updateMany({ asin: { '$in': asins } }, { processing: true });
+    exchange.publish({ asins }, { key });
+    await sleep(1000);
+    books = await Book.whereNeedsUpdate().exec();
+  }
+})().then(() => process.exit(0));
+
+process.on('unhandledRejection', (...errors) => {
+  console.dir(...errors);
+  process.exit(1);
+});
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
