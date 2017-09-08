9037095c55a8a3c4b640a97f19a05f990beffcdd
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-09-08 09:59:34 +0900

    add classify series worker

---
+++ worker/producers/classifySeries.js
@@ -0,0 +1,25 @@
+const classifySeries = require('../../lib/classifySeries');
+const Book = require('../../db/models/book');
+const Series = require('../../db/models/series');
+
+// const exchange = require('../amqp')();
+// const key = 'series.detail.get';
+
+(async () => {
+  const seriesIds = [];
+  const bookCriteria = Book.where({ seriesId: undefined });
+  while (await bookCriteria.count()) {
+    const book = await bookCriteria.findOne();
+    const { series } = await Series.firstOrCreate(classifySeries(book));
+    await series.addAsinIfNotIncludes(book.asin).save();
+    await book.set({ seriesId: series.id }).save();
+    console.log('%s -* %s', series.id, book.asin);
+    if (!seriesIds.includes(series.id)) { seriesIds.push(series.id); }
+  }
+  console.log({ seriesIds });
+})().then(() => process.exit(0));
+
+process.on('unhandledRejection', (...errors) => {
+  console.dir(...errors);
+  process.exit(1);
+});
